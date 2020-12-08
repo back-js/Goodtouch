dc60c32ff050ab58c3f45055c22bdd3dc93dc684
bask-js <back@yonsei.ac.kr>
2020-12-08 17:22:59 +0900

    발표전

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('makeup', views.image_upload_view, name = 'makeup'),
     path('', views.index, name='index'),
-    path('make', views.index2, name='make'),
+    path('makeup', views.image_upload_view, name = 'makeup'),
+    path('result', views.result, name = 'result'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/views.py
+++ app/views.py
@@ -7,7 +7,22 @@ import sys
 sys.path.insert(0, 'C:/Users/s_ois/PycharmProjects/goodtouch/modules/PSGAN/')
 from demo import main
 
+def index(request):
+    return render(request, 'index.html')
+
 def image_upload_view(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+
+            return render(request, 'makeup.html', {'form': form, 'img_obj': img_obj, 'img_result':data_url})
+    else:
+        form = ImageForm()
+    return render(request, 'makeup.html', {'form': form})
+
+def result(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
@@ -20,13 +35,7 @@ def image_upload_view(request):
             im_data = output.getvalue()
             data_url = 'data:image/jpg;base64,' + base64.b64encode(im_data).decode()
 
-            return render(request, 'makeup.html', {'form': form, 'img_obj': img_obj, 'img_result':data_url})
+            return render(request, 'result.html', {'form': form, 'img_obj': img_obj, 'img_result':data_url})
     else:
         form = ImageForm()
-    return render(request, 'makeup.html', {'form': form})
-
-def index(request):
-    return render(request, 'index.html')
-
-def index2(request):
-    return render(request, 'make.html')
+    return render(request, 'result.html', {'form': form})
